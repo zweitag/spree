936fcb43c93f5ba7a5ab02ed9ecd5242e4cb25ef
Ryan Bigg <radarlistener@gmail.com>
2012-04-20 14:39:39 -0400

    Rename AuthConfiguration to Auth::Configuration
    This is more sensible namespacing

--- auth/app/models/spree/auth_configuration.rb
+++ auth/app/models/spree/auth_configuration.rb
@@ -1,6 +1,8 @@
 module Spree
-  class AuthConfiguration < Preferences::Configuration
-    preference :registration_step, :boolean, :default => true
-    preference :signout_after_password_change, :boolean, :default => true
+  module Auth
+    class Configuration < Preferences::Configuration
+      preference :registration_step, :boolean, :default => true
+      preference :signout_after_password_change, :boolean, :default => true
+    end
   end
 end
