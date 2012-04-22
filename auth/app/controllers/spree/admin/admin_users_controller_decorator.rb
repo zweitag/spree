e7611793908bde179d35c44d42ad0e2d8d987450
Ryan Bigg <radarlistener@gmail.com>
2012-04-22 09:46:22 -0400

    Fix namespace specified for DestroyWithOrdersError inside Admin::UsersController decorator

--- auth/app/controllers/spree/admin/admin_users_controller_decorator.rb
+++ auth/app/controllers/spree/admin/admin_users_controller_decorator.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../../base_controller_decorator', __FILE__)
 Spree::Admin::UsersController.class_eval do
-  rescue_from Spree::User::DestroyWithOrdersError, :with => :user_destroy_with_orders_error
+  rescue_from Spree::Auth::UserExtensions::DestroyWithOrdersError, :with => :user_destroy_with_orders_error
 
   update.after :sign_in_if_change_own_password
 
