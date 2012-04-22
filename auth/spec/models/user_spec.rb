7e0032d04dd09b5b8b5cc6a98b0803d0d82d51e0
Ryan Bigg <radarlistener@gmail.com>
2012-04-22 10:46:30 -0400

    Remove tests for anonymous! user in Spree::User tests

--- auth/spec/models/user_spec.rb
+++ auth/spec/models/user_spec.rb
@@ -1,22 +1,7 @@
 require 'spec_helper'
 
-describe Spree::User do
+describe User do
   before(:all) { Spree::Role.create :name => 'admin' }
-
-  it 'should generate the reset password token' do
-    user = Factory.build(:user)
-    Spree::UserMailer.should_receive(:reset_password_instructions).with(user).and_return(double(:deliver => true))
-    user.send_reset_password_instructions
-    user.reset_password_token.should_not be_nil
-  end
-
-  context '#create' do
-    let(:user) { Factory.build(:user) }
-
-    it 'should not be anonymous' do
-      user.should_not be_anonymous
-    end
-  end
 
   context '#destroy' do
     it 'can not delete if it has completed orders' do
@@ -25,22 +10,6 @@ describe Spree::User do
       user = order.user
 
       lambda { user.destroy }.should raise_exception(Spree::User::DestroyWithOrdersError)
-    end
-  end
-
-  context 'anonymous!' do
-    let(:user) { Spree::User.anonymous! }
-
-    it 'should create a new user' do
-      user.new_record?.should be_false
-    end
-
-    it 'should create a user with an example.net email' do
-      user.email.should =~ /@example.net$/
-    end
-
-    it 'should be anonymous' do
-      user.should be_anonymous
     end
   end
 
